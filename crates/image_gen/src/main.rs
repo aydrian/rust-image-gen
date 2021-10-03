b1b5867b3cc9e93f20985e1dde77b4f6eed560dc
Aydrian Howard <aydrian@gmail.com>
2021-10-03 10:00:28 -0400

    first try with playwright

--- crates/image_gen/src/main.rs
+++ crates/image_gen/src/main.rs
@@ -3,12 +3,40 @@ use lamedh_http::{
     lambda::{lambda, Context, Error},
     IntoResponse, Request, Response,
 };
+use playwright::Playwright;
 
 #[lambda(http)]
 #[tokio::main]
 async fn main(_request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let playwright = Playwright::initialize().await.unwrap();
+    playwright.install_chromium().unwrap(); // Install browsers
+    let chromium = playwright.chromium();
+    let browser = chromium.launcher().headless(true).launch().await.unwrap();
+    let context = browser.context_builder().build().await.unwrap();
+    let page = context.new_page().await.unwrap();
+    page.set_viewport_size(playwright::api::Viewport {
+        width: 1200,
+        height: 630,
+    })
+    .await
+    .unwrap();
+    page.set_content_builder("<html><body><h1>Hello World</h1><p>This is a test</p></body></html>")
+        .set_content()
+        .await
+        .unwrap();
+    /*page.add_script_tag_builder(&format!(
+        "window.image = \"{}\";\nwindow.username = \"{}\";",
+        &res.avatar_url, &res.login
+    ))
+    .add_script_tag()
+    .await
+    .unwrap();*/
+    let data = page.screenshot_builder().screenshot().await.unwrap();
+
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(String::from("OK"))
+        .header("Content-Type", "image/png")
+        .header("Content-Length", data.len().to_string())
+        .body(base64::encode(data))
         .unwrap())
 }
